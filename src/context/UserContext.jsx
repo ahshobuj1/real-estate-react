789dc18ebddaba2d50ee8e852919ba68352556c7
A H Shobuj <ahshobuj09@gmail.com>
2024-08-10 21:03:40 +0600

    addety onAuthStateChange for get current value and updated navbar

--- src/context/UserContext.jsx
+++ src/context/UserContext.jsx
@@ -1,25 +1,54 @@
-import {createContext} from 'react';
+import {createContext, useEffect, useState} from 'react';
 import PropTypes from 'prop-types';
 import auth from '../firebase/firebase.config';
 import {
     createUserWithEmailAndPassword,
+    onAuthStateChanged,
     signInWithEmailAndPassword,
+    signOut,
 } from 'firebase/auth';
 
 export const AuthContext = createContext();
 
 const UserContext = ({children}) => {
+    const [user, setUser] = useState('');
+    const [loading, setLoading] = useState(true);
+
     // create user with email password
     const createUser = (email, password) => {
+        setLoading(true);
         return createUserWithEmailAndPassword(auth, email, password);
     };
 
     // Login user with email password
     const loginUser = (email, password) => {
+        setLoading(true);
         return signInWithEmailAndPassword(auth, email, password);
     };
 
-    const authInfo = {createUser, loginUser};
+    // Logout user
+    const logOutUser = (email, password) => {
+        setLoading(true);
+        return signOut(auth, email, password);
+    };
+
+    // Get current user
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            if (currentUser) {
+                console.log('current user', currentUser);
+                setUser(currentUser);
+                setLoading(false);
+            } else {
+                console.log('user not found');
+                setUser('');
+            }
+        });
+
+        return () => unsubscribe();
+    }, []);
+
+    const authInfo = {createUser, loginUser, logOutUser, user, loading};
 
     return (
         <AuthContext.Provider value={authInfo}>{children}</AuthContext.Provider>
--- src/layout/Navbar/Navbar.jsx
+++ src/layout/Navbar/Navbar.jsx
@@ -1,6 +1,16 @@
-import {NavLink} from 'react-router-dom';
+import {useContext} from 'react';
+import {Link, NavLink} from 'react-router-dom';
+import {AuthContext} from '../../context/UserContext';
 
 const Navbar = () => {
+    const {user, logOutUser} = useContext(AuthContext);
+
+    const handleLogOut = () => {
+        logOutUser()
+            .then(() => console.log('user logged out'))
+            .catch((err) => console.log(err.message));
+    };
+
     const navLinks = (
         <>
             <li>
@@ -60,36 +70,45 @@ const Navbar = () => {
             <div className="navbar-center hidden lg:flex">
                 <ul className="menu menu-horizontal px-1">{navLinks}</ul>
             </div>
+
             <div className="navbar-end">
-                <div className="dropdown dropdown-end">
-                    <div
-                        tabIndex={0}
-                        role="button"
-                        className="btn btn-ghost btn-circle avatar">
-                        <div className="w-10 rounded-full">
-                            <img
-                                alt="Tailwind CSS Navbar component"
-                                src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
-                            />
+                {user ? (
+                    <>
+                        <div className="dropdown dropdown-end">
+                            <div
+                                tabIndex={0}
+                                role="button"
+                                className="btn btn-ghost btn-circle avatar">
+                                <div className="w-10 rounded-full">
+                                    <img
+                                        alt="Tailwind CSS Navbar component"
+                                        src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.webp"
+                                    />
+                                </div>
+                            </div>
+                            <ul
+                                tabIndex={0}
+                                className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                                <li>
+                                    <a className="justify-between">
+                                        Profile
+                                        <span className="badge">New</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a>{user.email}</a>
+                                </li>
+                                <li>
+                                    <a onClick={handleLogOut}>Logout</a>
+                                </li>
+                            </ul>
                         </div>
-                    </div>
-                    <ul
-                        tabIndex={0}
-                        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li>
-                            <a className="justify-between">
-                                Profile
-                                <span className="badge">New</span>
-                            </a>
-                        </li>
-                        <li>
-                            <a>Settings</a>
-                        </li>
-                        <li>
-                            <a>Logout</a>
-                        </li>
-                    </ul>
-                </div>
+                    </>
+                ) : (
+                    <Link to="/login" className="btn btn-neutral btn-sm">
+                        Log In
+                    </Link>
+                )}
             </div>
         </div>
     );
